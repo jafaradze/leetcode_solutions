28578efdd54e024204b7d44cddce8561cc39eb52
Viktor Blinov <blinov.v20@gmail.com>
2025-10-13 17:27:32 +0400

    добавил задачу 2273

---
+++ 2273. Find Resultant Array After Removing Anagrams.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        res = [words[0]]
+        for i in range(1, len(words)):
+            if sorted(words[i]) != sorted(words[i - 1]):
+                res.append(words[i])
+        return res
